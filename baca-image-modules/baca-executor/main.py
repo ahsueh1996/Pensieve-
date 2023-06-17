2afa37b6cea650b54ec4f1b7a130c925d04fed53
ahsueh1996 <ahsueh1996@gmail.com>
2023-06-17 00:26:46 -0400

    add replace faces with apes and metadata output

--- baca-image-modules/baca-executor/main.py
+++ baca-image-modules/baca-executor/main.py
@@ -9,6 +9,8 @@ import torch
 import numpy as np
 import torchvision
 import PIL
+import matplotlib as plt
+import cv2
 
 OUTPUT_FOLDER = "outputs"
 os.makedirs(OUTPUT_FOLDER, exist_ok=True)
@@ -16,7 +18,7 @@ os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 TEST_IMG_FILE = os.path.join(OUTPUT_FOLDER,"test.png")
 
 print(f"*********** Pensive Generic Executor ***********")
-for lib in [np, torch, torchvision, PIL, json, base64, io, requests, argparse]:
+for lib in [np, torch, torchvision, PIL, plt, cv2, json, base64, io, requests, argparse]:
   print(f"{lib.__name__}=={lib.__version__ if hasattr(lib,'__version__') else ''}")
 imarray = np.random.rand(100,100,3) * 255
 im = Image.fromarray(imarray.astype('uint8')).convert('RGBA')
--- baca-image-modules/upload.py
+++ baca-image-modules/upload.py
@@ -3,16 +3,21 @@ import argparse
 from io import BytesIO
 import base64
 import json
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFilter
 import torch
 import torchvision.transforms as T
 from torchvision import models
 import numpy as np
+import cv2
+import random
 
 OUTPUT_FOLDER = "outputs"
 os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 
+METADATA_FILE = os.path.join(OUTPUT_FOLDER,"metadata.json")
+
 PROC_IMG_FILE = os.path.join(OUTPUT_FOLDER,"processed.png")
+SEG_IMG_FILE = os.path.join(OUTPUT_FOLDER,"segmentation.png")
 ORG_IMG_FILE = os.path.join(OUTPUT_FOLDER,"original.png")
 
 print(f"*********** Pensive Image Upload Preprocessing Module ***********")
@@ -20,13 +25,15 @@ parser = argparse.ArgumentParser(description='Pensieve Image Upload Preprocessin
 parser.add_argument('--i', dest='i', type=str, help='image in base64 (max dimensions est. 400x400)',default=None)
 parser.add_argument('--m', dest='m', type=str, help='metadata of image, json format',default="{}")
 args = parser.parse_args()
-imb64 = args.i
+imgb64 = args.i
 metadata = json.loads(args.m)
 
-if (imb64 == None):
-    im = Image.open("inputs/sample.jpg")
+if (imgb64 == None):
+    img = Image.open("inputs/sample.jpg")
 else:
-    im = Image.open(BytesIO(base64.b64decode(imb64)))
+    img = Image.open(BytesIO(base64.b64decode(imgb64)))
+
+orgimg = img.copy()
 
 
 # We will automatically segment instances of the image and tag each result
@@ -52,19 +59,26 @@ def decode_segmap(image, nc=21):
                (192, 128, 0), (64, 0, 128), (192, 0, 128), (64, 128, 128), (192, 128, 128),
                # 16=potted plant, 17=sheep, 18=sofa, 19=train, 20=tv/monitor
                (0, 64, 0), (128, 64, 0), (0, 192, 0), (128, 192, 0), (0, 64, 128)])
-
+  label_names = ["background",
+                 "airplane", "bike", "bird", "boat", "bottle",
+                 "bus", "car", "cat", "chair", "cow",
+                 "table", "dog", "horse", "motorbike", "person",
+                 "plant", "sheep", "sofa", "train", "monitor"]
   r = np.zeros_like(image).astype(np.uint8)
   g = np.zeros_like(image).astype(np.uint8)
   b = np.zeros_like(image).astype(np.uint8)
   
+  items = []
   for l in range(0, nc):
     idx = image == l
     r[idx] = label_colors[l, 0]
     g[idx] = label_colors[l, 1]
     b[idx] = label_colors[l, 2]
+    if (np.any(idx)):
+       items.append(label_names[l])
     
   rgb = np.stack([r, g, b], axis=2)
-  return rgb
+  return rgb, items
 
 
 def segment(net, img, show_pic=False, dev='cuda'):
@@ -78,15 +92,78 @@ def segment(net, img, show_pic=False, dev='cuda'):
   inp = trf(img).unsqueeze(0).to(dev)
   out = net.to(dev)(inp)['out']
   om = torch.argmax(out.squeeze(), dim=0).detach().cpu().numpy()
-  rgb = decode_segmap(om)
+  rgb, items = decode_segmap(om)
   #if show_pic: plt.imshow(rgb); plt.axis('off'); plt.show()
-  return rgb
+  return rgb, items
 
 
-npimg = segment(fcn, im, dev='cpu')
+npsegimg, items = segment(fcn, img, dev='cpu')
 
-im = Image.fromarray(np.uint8(npimg))
+segimg = Image.fromarray(np.uint8(npsegimg))
 
-im.save(PROC_IMG_FILE)
+npimg = np.array(img)
+
+grayimg = cv2.cvtColor(npimg, cv2.COLOR_BGR2GRAY)
+haar_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+faces_rect = haar_cascade.detectMultiScale(grayimg, scaleFactor=1.1, minNeighbors=9)
+print(faces_rect)
+
+mask = Image.new('L', img.size, 0)
+draw = ImageDraw.Draw(mask)
+apefiles = os.listdir("inputs/apes")
+apes = []
+for (x, y, w, h) in faces_rect:
+    cv2.rectangle(npimg, (x, y), (x+w, y+h), (0, 255, 0), thickness=2)
+    draw.rectangle([ (x,y), (x+w,y+h) ], fill=255)
+    apefile = random.choice(apefiles)
+    apeimg = Image.open(os.path.join('inputs','apes',apefile))
+    background = Image.new('RGBA', apeimg.size, (255,255,255))
+    alpha_composite = Image.alpha_composite(background, apeimg)
+    apeimg = alpha_composite.convert('RGB')
+    apeimg.thumbnail((w,h))
+    apes.append((apeimg,x,y,w,h,apefile))
+
+blurred = img.filter(ImageFilter.GaussianBlur(30))
+img.paste(blurred, mask=mask)
+
+# from matplotlib import pyplot as plt
+for ape in apes:
+  apeimg = ape[0]
+  x = ape[1]
+  y = ape[2]
+  w = ape[3]
+  h = ape[4]
+
+  npapeimg = np.array(apeimg)
+
+  mask = np.zeros(npapeimg.shape[:2], np.uint8)
+  backgroundModel = np.zeros((1, 65), np.float64)
+  foregroundModel = np.zeros((1, 65), np.float64)
+  rectangle = (w//6, h//6, w-w//6, h-h//6)
+  cv2.grabCut(npapeimg, mask, rectangle,  
+              backgroundModel, foregroundModel,
+              5, cv2.GC_INIT_WITH_RECT)
+  mask2 = np.where((mask == 2)|(mask == 0), 0, 1).astype('uint8')
+  image = npapeimg * mask2[:, :, np.newaxis]
+  # plt.imshow(mask2)
+  # plt.colorbar()
+  # plt.show()
+  mask2 = Image.fromarray(np.uint8(mask2*255))
+  img.paste(apeimg, (x,y), mask=mask2)
+
+
+
+img.save(PROC_IMG_FILE)
+segimg.save(SEG_IMG_FILE)
+orgimg.save(ORG_IMG_FILE)
+
+aperooturl='ipfs://bafybeihpjhkeuiq3k6nqa3fkgeigeri7iebtrsuyuey5y6vy36n345xmbi/'
+metadata = {"objects":items,
+            "faces": len(faces_rect),
+            "apes": [aperooturl+ape[-1].split('.')[0] for ape in apes]}
+
+with open(METADATA_FILE,"w") as f:
+  json.dump(metadata, f)
+
 
 print(f"Complete.")
